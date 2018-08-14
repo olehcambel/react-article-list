cf6fdd21a3791788454df5e7aae443fa57ea885d
olehcambel <nickcambel5@gmail.com>
2018-08-15 00:21:19 +0300

    add Immutable to AddComment, prepare to react-router

--- readme.md
+++ readme.md
@@ -1,7 +1,3 @@
 # TODO:
 
-- **DONE** handle Errors after fetching
-- **DONE** fix bugs while opening article
-- **DONE** create Record for comments, keep them in a structure the same as articles
-- **DONE** add comment's loader for article(opening), load once, show loader.. cache
-- add Immutable to AddComment
+- react-router
--- src/reducer/comments.js
+++ src/reducer/comments.js
@@ -15,18 +15,21 @@ const ReducerState = new Record({
 
 const defaultState = new ReducerState();
 
-// export default (commentsState = arrToMap(staticComments), action) => {
 export default (commentsState = defaultState, action) => {
   const { payload, uuid, response } = action;
+
   switch (action.type) {
     case types.COMMENT_ADD:
-      return {
-        ...commentsState,
-        [uuid]: { ...payload.comment, id: uuid }
-      };
+      return commentsState.setIn(
+        ['entities', uuid],
+        new CommentsRecord({ ...payload.comment, id: uuid })
+      );
 
     case types.LOAD_ARTICLE_COMMENTS + types.SUCCESS:
-      return commentsState.set('entities', arrToMap(response, CommentsRecord));
+      return commentsState.update('entities', entities =>
+        entities.merge(arrToMap(response, CommentsRecord))
+      );
+    // return commentsState.set('entities', arrToMap(response, CommentsRecord));
 
     default:
       return commentsState;
